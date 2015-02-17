847c19386ad916c207641df2fdf77edc6b0636e0
sergio <sergio@sergio2>
2015-02-17 22:02:29 +0100

    tested sensors on robot

--- src/read_sensors.py
+++ src/read_sensors.py
@@ -13,28 +13,32 @@ from sensor_msgs.msg import Range
 
 class ReadSensors():
 	def __init__(self):
+		
+		rate = float(rospy.get_param('~rate', '1.0'))
+		
+		max_range = 0.3
 		self.got_new_msg = False
 		self.range_msg1 = Range()
 		self.range_msg2 = Range()
 		self.range_msg3 = Range()
 		self.range_msg4 = Range()
 		self.range_msg1.min_range = 0.02
-		self.range_msg1.max_range = 1.0
+		self.range_msg1.max_range = max_range
 		self.range_msg1.radiation_type = self.range_msg1.ULTRASOUND
 		self.range_msg1.field_of_view = 0.7; # 40 degrees
 		self.range_msg1.header.frame_id = "/sergio/base_sonar1";
 		self.range_msg2.min_range = 0.02
-		self.range_msg2.max_range = 1.0
+		self.range_msg2.max_range = max_range
 		self.range_msg2.radiation_type = self.range_msg1.ULTRASOUND
 		self.range_msg2.field_of_view = 0.7; # 40 degrees
 		self.range_msg2.header.frame_id = "/sergio/base_sonar2";
 		self.range_msg3.min_range = 0.02
-		self.range_msg3.max_range = 1.0
+		self.range_msg3.max_range = max_range
 		self.range_msg3.radiation_type = self.range_msg1.ULTRASOUND
 		self.range_msg3.field_of_view = 0.7; # 40 degrees
 		self.range_msg3.header.frame_id = "/sergio/base_sonar3";
 		self.range_msg4.min_range = 0.02
-		self.range_msg4.max_range = 1.0
+		self.range_msg4.max_range = max_range
 		self.range_msg4.radiation_type = self.range_msg1.ULTRASOUND
 		self.range_msg4.field_of_view = 0.7; # 40 degrees
 		self.range_msg4.header.frame_id = "/sergio/base_sonar4";
@@ -50,23 +54,38 @@ class ReadSensors():
 		while not rospy.is_shutdown():
 			# Publish new data if we got a new message.
 			if self.got_new_msg:
+				self.range_msg1.header.stamp = rospy.get_rostime()
 				pub_sonar1.publish(self.range_msg1)
+				self.range_msg2.header.stamp = rospy.get_rostime()
 				pub_sonar2.publish(self.range_msg2)
+				self.range_msg3.header.stamp = rospy.get_rostime()
 				pub_sonar3.publish(self.range_msg3)
+				self.range_msg4.header.stamp = rospy.get_rostime()
 				pub_sonar4.publish(self.range_msg4)
 				self.got_new_msg = False
+			else:
+				self.range_msg1.header.stamp = rospy.get_rostime()
+				pub_sonar1.publish(self.range_msg1)
+				self.range_msg2.header.stamp = rospy.get_rostime()
+				pub_sonar2.publish(self.range_msg2)
+				self.range_msg3.header.stamp = rospy.get_rostime()
+				pub_sonar3.publish(self.range_msg3)
+				self.range_msg4.header.stamp = rospy.get_rostime()
+				pub_sonar4.publish(self.range_msg4)
+			
+			# Sleep for a while before publishing new messages. Division is so rate != period.
+			if rate:
+				rospy.sleep(1/rate)
+			else:
+				rospy.sleep(1.0)
 
 	# Ultrasone callback function.
 	def ultrasone_callback(self, msg):
 		# Get ranges.
-		self.range_msg1.range = msg.data[0]
-		self.range_msg2.range = msg.data[1]
-		self.range_msg3.range = msg.data[2]
-		self.range_msg4.range = msg.data[3]
-		self.range_msg1.header.stamp = rospy.get_rostime()
-		self.range_msg2.header.stamp = rospy.get_rostime()
-		self.range_msg3.header.stamp = rospy.get_rostime()
-		self.range_msg4.header.stamp = rospy.get_rostime()
+		self.range_msg1.range = min(msg.data[0], self.range_msg1.max_range)
+		self.range_msg2.range = min(msg.data[1], self.range_msg2.max_range)
+		self.range_msg3.range = min(msg.data[2], self.range_msg3.max_range)
+		self.range_msg4.range = min(msg.data[3], self.range_msg4.max_range)
 		self.got_new_msg = True
 
 # Main function.
